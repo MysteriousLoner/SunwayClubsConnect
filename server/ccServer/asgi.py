dfb7b51924537d4cb157676e4a6506c2faa5dee2
MysteriousLoner <leeyang031110@gmail.com>
2025-04-05 15:19:15 +0800

    restructure init, add django

---
+++ server/ccServer/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for ccServer project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ccServer.settings')
+
+application = get_asgi_application()
