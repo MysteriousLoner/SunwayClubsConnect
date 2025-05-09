34843d292b76854c9811737cacbfd36d19c7c435
MysteriousLoner <leeyang031110@gmail.com>
2025-05-10 02:47:45 +0800

    login

--- client/src/routes/+layout.svelte
+++ client/src/routes/+layout.svelte
@@ -1,19 +1,6 @@
-<script>
-	// imports
-	import StickyContent from '../components/StickyContent.svelte';
-	import "../app.css";
-
-	// svelte takes static directry as root
-	let images = [
-	  '/42KLLogo.png',
-	  '/SCLogo.png',
-	  '/studentLifeLogo.png',
-	  '/SULogo.png',
-	  '/SUSCLogo.png'
-	];
+<script lang="ts">
+	import '../app.css';
+	let { children } = $props();
 </script>
 
-<main class="h-screen">
-	<!-- Dynamically renders content from `page.svelte` -->
-	<slot /> 
-</main>
+{@render children()}
--- client/src/routes/kyec/+page.svelte
+++
@@ -1,1 +0,0 @@
-<h1>KYEC Game</h1>
