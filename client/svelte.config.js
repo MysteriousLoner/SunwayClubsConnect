a214f4650bbf4717a5ab5f559b082ec6a101bf86
leeyang-mac <leeyang031110@gmail.com>
2025-05-13 14:50:47 +0800

    use netlify adapter

--- client/svelte.config.js
+++ client/svelte.config.js
@@ -1,7 +1,7 @@
-// import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 // import { preprocess } from 'svelte/compiler';
-import adapter from '@sveltejs/adapter-node';
+// import adapter from '@sveltejs/adapter-node';
 
 const config = { kit: { adapter: adapter() }, preprocess: vitePreprocess };
 
