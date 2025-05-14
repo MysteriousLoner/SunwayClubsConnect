5b4a41dd68bd71974e2f57b6310b6491d9e8ca7a
cyu-xian <thg7091@gmail.com>
2025-05-14 22:32:20 +0800

    added charts in admin dashboard

---
+++ client/src/lib/ChartBox.svelte
@@ -0,0 +1,14 @@
+<script>
+  import { Chart, Card } from 'flowbite-svelte';
+
+  export let title = 'Chart Title';
+  export let options;
+</script>
+
+<Card class="w-full h-full">
+  <div class="flex justify-between items-center mb-4">
+    <h2 class="text-lg font-bold text-white py-2 px-2">{title}</h2>
+  </div>
+
+  <Chart {options} class="w-full h-64" />
+</Card>
